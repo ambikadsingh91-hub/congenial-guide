3619724872815369c97f5371558f2ab2847d4e67
ambikadsingh91-hub <ambikadsingh91@gmail.com>
2026-09-18 16:42:35 +0530

    Integrate Firebase SDK and configuration
    Added Firebase scripts and initialization to the listing update process.

--- script.js
+++ script.js
@@ -1029,4 +1029,21 @@ async function confirmPurchase() {
         transaction.update(
           listingRef,
           {
-   
+   <script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore-compat.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyD89JlsSXpaAse-ZgUXnlUIqqlTPe-6Bys",
+    authDomain: "lotapimain.firebaseapp.com",
+    projectId: "lotapimain",
+    storageBucket: "lotapimain.firebasestorage.app",
+    messagingSenderId: "909823166989",
+    appId: "1:909823166989:web:288001e0e37c2fa7282344"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+<script src="script.js"></script>
